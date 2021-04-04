42987d301c0a6166541e18c8e8a1aedd65cd83a6
shuhei05 <qoo0228y@gmail.com>
2021-04-04 06:00:22 +0000

    henkou

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
- root to:'homes#top'
+  root 'post_images#index'
  resources :post_images, only: [:new, :create, :index, :show, :destroy]do
      resource :favorites, only: [:create, :destroy]
 
